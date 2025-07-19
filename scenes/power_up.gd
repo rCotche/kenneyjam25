1ec778833888b68f12eccca8b5fbdd1cff02342e
rCotche <roshan.cotche@gmail.com>
2025-07-19 07:40:28 +0400

    camera fix, pick up area fix, healthComponent scene created

--- scenes/power_up.gd
+++ scenes/power_up.gd
@@ -6,17 +6,23 @@ var available_options := ['heal', 'damage']
 #randomize power up
 var type = available_options[randi()%len(available_options)]
 
-func _process(delta: float) -> void:
-	#to fix
-	#rotation += rotation_speed * delta
+#func _process(delta: float) -> void:
+	##to fix
+	##rotation += rotation_speed * delta
+	#pass
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		apply_effect(body)
+		print("pick up")
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		apply_effect(body)
+		print("nope")
+
+func apply_effect(_player) -> void:
+	# Exemple : augmenter une stat du joueur
+	# player.add_mana(10)
 	pass
-
-func _on_area_entered(area: Area3D) -> void:
-	#ui appear
-	print("pick up")
-	#queue_free()
-
-
-func _on_area_exited(area: Area3D) -> void:
-	#ui disappear
-	print("nope")

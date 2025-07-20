99916ced382547baba649d0aee97a0d4616d18bb
rCotche <roshan.cotche@gmail.com>
2025-07-20 14:18:04 +0400

    pickup animation in progress

--- scenes/power_up.gd
+++ scenes/power_up.gd
@@ -1,29 +1,24 @@
 extends Area3D
 
 #POWER UP DESCRIPTION
-var rotation_speed: int = 5
+@export var rotation_speed: int = 5
 #power up type
 var available_options := ['heal', 'damage']
 #randomize power up
 var type = available_options[randi()%len(available_options)]
 
 @export var points : int = 0
-
+@onready var animationplayer = $AnimationPlayer
 #
 signal powerup_near(near: bool)
 
-#func _process(delta: float) -> void:
-	##to fix
-	##rotation += rotation_speed * delta
-	#pass
+func _process(delta: float) -> void:
+	rotation.y += rotation_speed * delta
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		powerup_near.emit(true)
 		Globals.score_amount += points
-		queue_free()
-
-func apply_effect(_player) -> void:
-	# Exemple : augmenter une stat du joueur
-	# player.add_mana(10)
-	pass
+		print("av")
+		animationplayer.play("pickup")
+		print("ap")
--- scenes/ui.gd
+++ scenes/ui.gd
@@ -6,6 +6,7 @@ extends CanvasLayer
 func _ready() -> void:
 	#connect le script Globals
 	Globals.connect("stat_change", update_score_text)
+	update_score_text()
 
 func update_score_text() -> void :
 	score_label.text = str(Globals.score_amount)
